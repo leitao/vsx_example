11bd767866ecb6e95327267744d2fbd5556b3239
Breno Leitao <breno.leitao@gmail.com>
2018-03-03 15:04:03 -0500

    Keep just the VMX version

--- Makefile
+++ Makefile
@@ -1,3 +1,2 @@
 all:
-	gcc -O0 -g vmx.c -o vec
 	gcc -O0 -g vmx.c -DVMX -o vmx
--- vmx.c
+++ vmx.c
@@ -9,49 +9,36 @@ typedef float v4sf __attribute__ ((vector_size(16)));
 
 int add_mul() {
 
-        v4sf A, B, C, D;
+        v4sf A, B, C, D, E, F, G;
         long long i, z, y;
 
         long long a = 0;
 
         A = (v4sf) {20, 2, 9, 100};
         B = (v4sf) {2, 10, 3, 2};
+        G = A;
 
         /* Loop until it overflows */
         for(z = 0; z < LOOPMAX; z++){
-            a++;
-#ifdef VMX
             for (y = 0; y < LOOP2; y++)
-                C = vec_add(A, B);
-#else
+                C = vec_add(A, G);
 
             for (y = 0; y < LOOP2; y++)
-                for(i = 0; i < 4; i++)
-                    C[i] = A[i] + B[i];
-#endif
+                D = vec_mul(A, C); /* VSX intruction implemented on Power7 ISA (2.06) */
 
-
-#ifdef VMX
             for (y = 0; y < LOOP2; y++)
-                D = vec_mul(A, B); /* VSX intruction implemented on Power7 ISA (2.06) */
-#else /* Fallback to non-vectorized implementation */
-            for (y = 0; y < LOOP2; y++)
-                for(i = 0; i < 4; i++)
-                    D[i] = A[i] * B[i];
-#endif
+                E = vec_and(A, D); /* VSX intruction implemented on Power7 ISA (2.06) */
 
+            for (y = 0; y < LOOP2; y++)
+                F = vec_div(E, B); /* VSX intruction implemented on Power7 ISA (2.06) */
+
+            for (y = 0; y < LOOP2; y++)
+                G = vec_xor(F, E); /* VSX intruction implemented on Power7 ISA (2.06) */
         }
-
-        printf("X: %lx\n", a);
 
         printf("Result vector add:\n");
         for (i = 0; i < 4; i++)
-                printf("v[%d]=%f \n", i,C[i]);
-        printf("\n");
-
-        printf("Result vector multiply: \n");
-        for (i = 0; i < 4; i++)
-                printf("v[%d]=%f \n", i, D[i]);
+                printf("v[%d]=%f \n", i, G[i]);
         printf("\n");
 
         return EXIT_SUCCESS;
